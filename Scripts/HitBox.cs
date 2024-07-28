33dc89c84dc11be8211d7564aedb2cbca65a672f
ash1ta <haiyu19931121@163.com>
2024-07-28 16:51:24 +0800

    finished

--- Scripts/HitBox.cs
+++ Scripts/HitBox.cs
@@ -20,6 +20,16 @@ public partial class HitBox : Area2D
         HurtBox hurtBox = area as HurtBox;
         EmitSignal(SignalName.Hit, hurtBox);
         hurtBox?.EmitSignal(HurtBox.SignalName.Hurt, this);
+        Shake();
+    }
+
+    public async void Shake()
+    {
         _gameGlobal.ShakeCamera(8, 0.8f);
+        Engine.TimeScale = 0.1;
+        var timer = GetTree().CreateTimer(0.1, true, false, true);
+
+        await ToSignal(timer, Timer.SignalName.Timeout);
+        Engine.TimeScale = 1;
     }
 }
